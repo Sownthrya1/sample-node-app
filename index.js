e8f2d00d54160a1d324e931dd791a99ee3aaa73d
Sownthrya1 <sownthrya@bytescare.com>
2025-06-13 12:36:59 +0530

    Deploy test2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Sownthrya Sample node app");
+  res.end("Sownthrya sample node app");
 });
 
 server.listen(PORT, () => {
